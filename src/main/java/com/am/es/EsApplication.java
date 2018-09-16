d126ea05601b3d43b1c357e965748b7fae492078
me <sam.ye@all-dream.com>
2018-09-16 14:03:55 +0800

    es search Commit

--- src/main/java/com/am/es/EsApplication.java
+++ src/main/java/com/am/es/EsApplication.java
@@ -1,12 +1,10 @@
 package com.am.es;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
